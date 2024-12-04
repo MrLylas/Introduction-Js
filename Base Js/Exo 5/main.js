b95aabd5e6ebc3d26dbaeca77910e6ab8236e3ec
MrLylas <delmas.denilson.pro@gmail.com>
2024-12-04 16:10:04 +0100

    Add like function

--- Base Js/Exo 5/main.js
+++ Base Js/Exo 5/main.js
@@ -1,33 +1,73 @@
+// Importer le fichier quotes
 
-let quotes = {
-    "Serge Karmarov":"Barrez vous ***** de mimes.",
-    "Georges Abitbol":"Le train de tes injures roule sur le rail de mon indifférence.",
-    "Hubert Bonisseur de la Bath":"Ca ne prenait pas beaucoup plus de temps.",
-    "Moulinier":"L'habit ne fait pas le moine, mais il fait l'agent...Même non titularisé.",
-    "Orson Welles":"C'est du vol c'est du plagiat. J'aime pas trop les voleurs et..."
-    };
+import {quotes} from './quotes.js'
 
-     
+// Définir la constante contenant nos citations
+
     const quotesContainer = document.querySelector('.container');
-    const citation = document.querySelector('.quotes');
-    const auteur = document.querySelector('.author');
-
-
-    function displayAuthor(){
-        for(let auteurs in quotes){
-            // citations
-            const citationElement = document.createElement('p');
-            citationElement.classList.add('quotes');
-            // auteurs
-            const auteurElement = document.createElement('p');
-            auteurElement.classList.add('author');
-            
-            citationElement.innerHTML = `${quotes[auteurs]}`;
-            auteurElement.innerHTML = `${auteurs}`;
-
-            quotesContainer.appendChild(citationElement);
-            quotesContainer.appendChild(auteurElement);
-            }
-        };
     
-    displayAuthor();
+    //Application d'un forEach sur chaque citations : 
+
+            quotes.forEach(quote=> {
+
+                // Création de div pour chaque citation
+
+                const quoteBox = document.createElement('div');
+
+    //associer Auteur à quoteBox
+
+                quoteBox.classList.add('quotes')
+                    const authorBox = document.createElement('h3');
+                        const authorText = document.createTextNode(quote["author"]);
+                        quoteBox.appendChild(authorBox);
+                        authorBox.appendChild(authorText);
+
+    //associer citation à quoteBox
+
+                    const contentBox = document.createElement('p');
+                        const contentText = document.createTextNode(quote["content"]);
+                        quoteBox.appendChild(contentBox);
+                        contentBox.appendChild(contentText);
+
+    //associer Titre à quoteBox
+                        
+                    const textBox = document.createElement('p');
+                        const titleText = document.createTextNode(quote["title"]);
+                        quoteBox.appendChild(textBox);
+                        textBox.appendChild(titleText);
+
+    //Implanter quoteBox à quotesContainer
+
+                quotesContainer.appendChild(quoteBox);
+
+                
+            }); 
+
+    //Bouton fav
+
+            const favButtons = document.querySelectorAll('.favBtn')
+
+    //ajout de la fonction click sur le favbutton
+
+            favButtons.forEach(favButton => {
+                favButton.addEventListener('click',function(){
+                    like(favButton);
+                });
+
+        });
+        
+            console.log(authorText);
+
+    //Si favButton est cliqué 
+
+            function like(favButton){
+                if(favButton.classList.contains('fa-solid')){
+                    favButton.class.replace('fa-regular','fa-regular');
+                    favButton.class.remove('favorite');
+                }else{
+                    favButton.class.replace('fa-regular','fa-regular');
+                    
+                }
+            }
+
+<i class="fa-regular fa-heart"></i>
